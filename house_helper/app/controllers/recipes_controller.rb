6559be4e062916a2d88cb58f76c8f44b333f8679
Terrie Yang <terrieyang@berkeley.edu>
2017-10-07 13:34:40 -0700

    root to home and working on ingredients to recipes

--- house_helper/app/controllers/recipes_controller.rb
+++ house_helper/app/controllers/recipes_controller.rb
@@ -1,8 +1,8 @@
 class RecipesController < ApplicationController
-	#list all recipes
+	#https://rbudiharso.wordpress.com/2010/07/07/dynamically-add-and-remove-input-field-in-rails-without-javascript/#jp-carousel-119
 
 	autocomplete :ingredient, :name, :full => true
-	
+
 	def index
 		@recipes = Recipe.all
 	end
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
 	#form for a new recipe
 	def new
 		@recipe = Recipe.new
-		@hash = {}
+		@recipe.ingredients.build
 	end
 
 	#edit a recipe
@@ -25,21 +25,50 @@ class RecipesController < ApplicationController
 
 	#update recipe
 	def update
-	    @recipe = Recipe.find(params[:id])
-	    if @recipe.update(recipe_params)
-	      redirect_to @recipe, notice: "Recipe successfully updated!"
-	    else
-	      render :edit
-	    end
+		@recipe = Recipe.find(params[:id])
+		if params[:add_ingredient]
+			@recipe.ingredients.build
+    	# # rebuild the ingredient attributes that doesn't have an id
+    	# 	unless params[:recipe][:ingredients_attributes].blank?
+	  		# 	for attribute in params[:recipe][:ingredients_attributes]
+	    # 			@recipe.ingredients.build(attribute.last.except(:_destroy)) unless attribute.last.has_key?(:id)
+	  		# 	end
+    	# 	end
+     #  		# add one more empty ingredient attribute
+     #  		@recipe.ingredients.build
+    	elsif params[:remove_ingredient]
+      		# # collect all marked for delete ingredient ids
+      		# removed_ingredients = params[:recipe][:ingredients_attributes].collect { |i, att| att[:id] if (att[:id] && att[:_destroy].to_i == 1) }
+      		# # physically delete the ingredients from database
+      		# Ingredient.delete(removed_ingredients)
+      		# flash[:notice] = "Ingredients removed."
+      		# for attribute in params[:recipe][:ingredients_attributes]
+      		# 	# rebuild ingredients attributes that doesn't have an id and its _destroy attribute is not 1
+        # 		@recipe.ingredients.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
+      		# end
+    	else
+		 
+		    if @recipe.update(recipe_params)
+		      redirect_to @recipe, notice: "Recipe successfully updated!"
+		    else
+		      render :edit
+		    end
+		 end
 	 end
 
 	#creates the new reicpe
 	def create
 		@recipe = Recipe.new(recipe_params)
-		if @recipe.save
-			redirect_to @recipe, notice: 'Recipe successfully created!'
+		if params[:add_ingredient]
+			@recipe.ingredients.
+		elsif params[:remove_ingredient]
+			# nested model that have _destroy attribute = 1 automatically deleted by rails
+		else
+			if @recipe.save
+				redirect_to @recipe, notice: 'Recipe successfully created!'
 		# else
 		# 	render :new, :alert @recipes.errors
+			end
 		end
 	end
 
--- house_helper/config/routes.rb
+++ house_helper/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :recipes
 
+  root 'home#index'
+
   get 'home' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
