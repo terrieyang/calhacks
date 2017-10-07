154cc19f09b0183a6cf46e61854e0d11af92a8c6
Terrie Yang <terrieyang@berkeley.edu>
2017-10-07 01:33:59 -0700

    add framework for recipes controller

--- house_helper/app/controllers/recipes_controller.rb
+++ house_helper/app/controllers/recipes_controller.rb
@@ -1,2 +1,27 @@
 class RecipesController < ApplicationController
+
+	#list all recipes
+	def index
+	end
+
+	#show info for one recipe
+	def show
+	end
+
+	#form for a new recipe
+	def new
+	end
+
+	#edit a recipe
+	def edit
+	end
+
+	#creates the new reicpe
+	def create
+	end
+
+	#delete recipe
+	def destroy
+	end
+
 end
