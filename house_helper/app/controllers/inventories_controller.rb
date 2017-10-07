3afe7278f629ff0de7f6771ed71ae15755c6ab25
Andrew Tunggal <a.r.tunggal@gmail.com>
2017-10-07 04:22:20 -0700

    Inventory stuffs

---
+++ house_helper/app/controllers/inventories_controller.rb
@@ -0,0 +1,52 @@
+class InventoriesController < ApplicationController
+
+	def index
+		@inventories = Inventory.all
+	end
+
+	#show info for inventory
+	def show
+		@inventory = Inventory.find(params[:id])
+	end
+
+	#form for a new inventory
+	def new
+		@inventory = Inventory.new
+	end
+
+	#edit an inventory
+	def edit
+		@inventory = Inventory.find(params[:id])
+	end
+
+	#update an inventory
+	def update
+		@inventory = Inventory.find(params[:id])
+    	if @inventory.update(inventory_params)
+	      redirect_to @inventory, notice: "Inventory successfully updated!"
+	    else
+	      render :edit
+    	end
+	end
+
+	#creates the new inventory
+	def create
+		@inventory = Inventory.new(inventory_params)
+		if @inventory.save
+			redirect_to @inventory, notice: 'Inventory successfully created!'
+			render :show
+		# else
+		# 	render :new, :alert @inventories.errors
+		end
+	end
+
+	#delete inventory
+	def destroy
+		@inventory.destroy
+	end
+
+	def inventory_params
+		params.require(:inventory).permit(:name)
+	end
+
+end
--- house_helper/db/schema.rb
+++ house_helper/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007085645) do
+ActiveRecord::Schema.define(version: 20171007101831) do
+
+  create_table "inventories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
