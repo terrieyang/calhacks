806e3df8ca6d9f733ad0f13477cb75bd6b6580fb
Terrie Yang <terrieyang@berkeley.edu>
2017-10-07 18:58:40 -0700

    messed up stuff

--- house_helper/app/controllers/recipes_controller.rb
+++ house_helper/app/controllers/recipes_controller.rb
@@ -14,7 +14,8 @@ class RecipesController < ApplicationController
 	#form for a new recipe
 	def new
 		@recipe = Recipe.new
-		@hash = {}
+		@ingredients = Ingredient.all
+		@recipe.ingredients.build 
 	end
 
 	#edit a recipe
@@ -24,23 +25,67 @@ class RecipesController < ApplicationController
 
 	#update recipe
 	def update
-	    @recipe = Recipe.find(params[:id])
-	    if @recipe.update(recipe_params)
-	      redirect_to @recipe, notice: "Recipe successfully updated!"
+		@recipe = Recipe.find(params[:id])
+	    if params[:add_ingredient]
+	    	# rebuild the ingredient attributes that doesn't have an id
+	    	
+	      # add one more empty ingredient attribute
+	      @recipe.ingredients.build
+	    elsif params[:remove_ingredient]
+	      # collect all marked for delete ingredient ids
+	      removed_ingredients = params[:recipe][:ingredients_attributes].collect { |i, att| att[:id] if (att[:id] && att[:_destroy].to_i == 1) }
+	      # physically delete the ingredients from database
+	      Ingredient.delete(removed_ingredients)
+	      flash[:notice] = "Ingredients removed."
+	      for attribute in params[:recipe][:ingredients_attributes]
+	      	# rebuild ingredients attributes that doesn't have an id and its _destroy attribute is not 1
+	        @recipe.ingredients.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
+	      end
 	    else
-	      render :edit
+	      # save goes like usual
+	      if @recipe.update_attributes(params[:recipe])
+	        flash[:notice] = "Successfully updated recipe."
+	        redirect_to @recipe and return
+	      end
 	    end
-	 end
+	    render :action => 'edit'
+	  end
+	    # @recipe = Recipe.find(params[:id])
+	    # @ingredients = Ingredients.all
+	    # if @recipe.update(recipe_params)
+	    #   redirect_to @recipe, notice: "Recipe successfully updated!"
+	    # else
+	    #   render :edit
+	    # end
+
 
 	#creates the new reicpe
 	def create
 		@recipe = Recipe.new(recipe_params)
-		if @recipe.save
-			redirect_to @recipe, notice: 'Recipe successfully created!'
-		# else
-		# 	render :new, :alert @recipes.errors
-		end
-	end
+	    if params[:add_ingredient]
+	      # add empty ingredient associated with @recipe
+	      puts("hello")
+	      @recipe.ingredients.push(Ingredient.new)
+	      puts(@recipe.ingredients.count)
+	    elsif params[:remove_ingredient]
+	      # nested model that have _destroy attribute = 1 automatically deleted by rails
+	    else
+	      # save goes like usual
+	      if @recipe.save
+	        flash[:notice] = "Successfully created recipe."
+	        redirect_to @recipe and return
+	      end
+	    end
+	    render :action => 'new'
+	  end
+		# @recipe = Recipe.new(recipe_params)
+		# @ingredients = Ingredients.all
+		# if @recipe.save
+		# 	redirect_to @recipe, notice: 'Recipe successfully created!'
+		# # else
+		# # 	render :new, :alert @recipes.errors
+		# end
+
 
 	#delete recipe
 	def destroy
